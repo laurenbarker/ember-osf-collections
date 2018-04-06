7601e78f774e5566249d58d2dab930c37245787e
Lauren Barker <lauren@cos.io>
2018-04-06 14:34:49 -0400

    Use ember-i18n

---
+++ app/application/controller.ts
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+import OSFAgnosticAuthControllerMixin from 'ember-osf/mixins/osf-agnostic-auth-controller';
+
+/**
+ * @module ember-osf-collections
+ * @submodule controllers
+ */
+
+/**
+ * @class Application Controller
+ * @extends Ember-OSF.OSFAgnosticAuthControllerMixin
+ */
+export default Controller.extend(OSFAgnosticAuthControllerMixin, {
+
+});
--- app/application/route.ts
+++ app/application/route.ts
@@ -2,11 +2,12 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class Application extends Route.extend({
-  // anything which *must* be merged to prototype here
-    intl: service(),
-    beforeModel() {
-        return this.get('intl').setLocale('en-us');
-    }
+    // anything which *must* be merged to prototype here
+    i18n: service(),
+
+    afterModel() {
+        this.set('i18n.locale', 'en');
+    },
 }) {
-  // normal class body definition here
+    // normal class body definition here
 }
