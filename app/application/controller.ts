276fb08c8f0a8b4c66f5ffac8a5f222b150f5b41
Lauren Barker <lauren@cos.io>
2018-04-16 12:07:57 -0400

    Inject theme service in application controller

--- app/application/controller.ts
+++ app/application/controller.ts
@@ -1,8 +1,12 @@
 import Controller from '@ember/controller';
 
+import { service } from '@ember-decorators/service';
+
 import OSFAgnosticAuthControllerMixin from 'ember-osf/mixins/osf-agnostic-auth-controller';
 
-export default class Application extends Controller.extend(OSFAgnosticAuthControllerMixin) { }
+export default class Application extends Controller.extend(OSFAgnosticAuthControllerMixin) {
+    @service theme;
+}
 
 declare module '@ember/controller' {
     interface ControllerRegistry {
